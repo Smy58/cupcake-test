c7eb7dd0a2ac19ebd43491ecf6c3781bfa26f5bb
Ilshat Osmanov <190103274@stu.sdu.edu.kz>
2023-11-18 01:37:11 +0600

    Start screen doesn't have an Up button test

--- app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -3,10 +3,12 @@ package com.example.cupcake.test
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.example.cupcake.CupcakeApp
 import com.example.cupcake.CupcakeScreen
+import com.example.cupcake.R
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
@@ -31,6 +33,12 @@ class CupcakeScreenNavigationTest {
     @Test
     fun cupcakeNavHost_verifyStartDestination() {
         navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
+
+    @Test
+    fun cupcakeNavHost_verifyBackNavigationNotShownOnStartOrderScreen() {
+        val backText = composeTestRule.activity.getString(R.string.back_button)
+        composeTestRule.onNodeWithContentDescription(backText).assertDoesNotExist()
     }
 
 }
